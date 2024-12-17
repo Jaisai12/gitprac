ac0a0f1440cc50668aa02108fb07f6a89a0b1a55
JaisaikumarVelupula <velupulajaisaikumar@gmail.com>
2024-12-17 17:08:15 +0530

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/* function Jai() {
+  return (
+    <div>
+      <h1>I am jaisaikunar</h1>
+    </div>
+  );
+} */
+
+/* class Jai extends React.Component {
+  render() {
+    return <h1>Jai</h1>;
+  }
+} */
+
+/* function Buttonclick() {
+  let [count, setCount] = useState(0);
+  const handleClicks = (num) => {
+    setCount(count + num);
+  };
+  return (
+    <div>
+      <p>Number of clicks={count}</p>
+      <button onClick={() => handleClicks(1)}> Click Here</button>
+      <button onClick={() => handleClicks(-1)}> Click Here</button>
+    </div>
+  );
+}
+ */
+
+/* class Buttonclick extends React.Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+  handleClicks = (num) => {
+    this.setState((prevState) => ({ count: prevState.count + num }));
+  };
+  render() {
+    return (
+      <div>
+        <p>Number of clicks={this.state.count}</p>
+        <button onClick={() => this.handleClicks(1)}> Click Here</button>
+        <button onClick={() => this.handleClicks(-1)}> Click Here</button>
+      </div>
+    );
+  }
+} */
+
+class Buttonclick extends React.Component {
+  constructor() {
+    super();
+    this.state = { clicks: 0 };
+  }
+  handleClicks = () => {
+    this.setState((prevState) => ({ clicks: prevState.clicks + 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <p>Number of clicks : {this.state.clicks}</p>
+        <button
+          onClick={() => {
+            this.handleClicks();
+          }}
+        >
+          Click me{" "}
+        </button>
+      </div>
+    );
+  }
+}
+
+const re = ReactDOM.createRoot(document.getElementById("root"));
+re.render(<Buttonclick />);
---
+++ src/jai.jsx
@@ -0,0 +1,12 @@
+/* function Jai(props) {
+  const { Name } = props;
+  console.log(Name);
+  return (
+    <div>
+      <h1>{Name}</h1>
+    </div>
+  );
+}
+
+export default Jai;
+ */
